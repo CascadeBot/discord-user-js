6815d8e3ce5c98c36766c2ff162849393bdfe677
Jelle van Snik <jellevs@gmail.com>
2020-04-29 13:12:12 +0200

    Cleaned up test file

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-const { DiscordUser, DiscordBot } = require("./index");
+const { DiscordBot } = require("./index");
 
 const botClient = new DiscordBot(require("./config.json").bot);
 
-botClient.getBot().then(({body}) => {
-    console.log("Bot user: ", body);
+botClient.getBot().then((res) => {
+    console.log("Bot user one: ", res.body);
 }).catch((err) => {
     console.error("Something went wrong: ", err);
 });
